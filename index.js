f11819191d6ca1ac385220f23f9ca657bc98200a
Shakinah Asante <54449230+4-8ubot@users.noreply.github.com>
2024-05-22 10:11:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,16 @@
  class ByteCord {}
 
  class Theme {
-  
+  #main_color = 0x2f3542
+  #secondary_color = 0x57606f
  }
  
  class LightTheme extends Theme {
   constructor() {
    super();
-  }
+   this.#main_color = 0xffffff
+    this.#secondary_color = 0xf1f2f6
+  } 
  }
  
 })
