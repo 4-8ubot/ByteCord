5e0309fdd05f309f4993e8c65c807b61d8cbb76b
Shakinah Asante <54449230+4-8ubot@users.noreply.github.com>
2024-05-22 13:05:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@
  "use strict";
 
  class ByteCord {
-  constructor() {
-   this.theme = new DefaultTheme()
-  }
- }
 
-class Themes {
  public DefaultTheme = class {
   #main_color = 0x2f3542
   #secondary_color = 0x57606f 
+ }
+  constructor() {
+   this.theme = new DefaultTheme()
+  }
  }
 }
 })
