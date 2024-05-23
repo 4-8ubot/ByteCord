338828eb5f1ea2cbb872e8e5204ec10301c8e9b0
Shakinah Asante <54449230+4-8ubot@users.noreply.github.com>
2024-05-23 12:47:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 (function main() {
- "use strict";
+"use strict";
 
- class ByteCord {
+class ByteCord {
+    constructor() {
+        console.log("initiated bytecord plugin...")
 
- public DefaultTheme = class {
-  #main_color = 0x2f3542
-  #secondary_color = 0x57606f 
- }
-  constructor() {
-   this.theme = new DefaultTheme()
-  }
- }
+        if(!navigator.onLine) {
+            ByteCord.UICore.displayPrompt("hi");
+            throw new Error("no internet connection");
+        }
+    }
+
+    static UICore = class {
+        /* prompt method: shows a temporary message on screen */
+        static displayPrompt = function() {
+            var prompt_div, prompt_text;
+            div = $("<div></div>");
+            text = $("<p>").appendTo(div);
+        }
+    }
+
+    static EventManager = class {
+
+    }
+
+    static Themes = new (function() {
+        this.DefaultTheme = class {
+            #main_color = 0x2f3542;
+            #secondary_color = 0x57606;
+        }
+        this.LightTheme =  class extends this.DefaultTheme {
+            #main_color = 0xffffff;
+            #secondary_color = 0xdfe4ea;
+        } 
+    })();
 }
 })
